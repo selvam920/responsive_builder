9f7f3371b49b413239e2e70d6ea33a1f95c765b6
selvam920 <skssoftware@live.com>
2026-05-20 10:31:57 +0530

    support web

--- lib/src/helpers/device_width.dart
+++ lib/src/helpers/device_width.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 import 'dart:ui';
+import 'package:flutter/foundation.dart';
 
 double deviceWidth(Size size) {
+  if (kIsWeb) {
+    return size.width;
+  }
   if (Platform.isMacOS || Platform.isWindows || Platform.isLinux) {
     return size.width;
   }
